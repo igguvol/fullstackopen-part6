56685eedcc6ad651b8e22222acd1c637cb651c36
astu <astu@kapsi.fi>
2018-10-25 18:13:07 +0300

    about-page styles

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -28,16 +28,27 @@ const AnecdoteList = ({ anecdotes }) => {
 }
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h2>About anecdote app</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-8" style={{margin:'auto'}}>
+        <p>According to Wikipedia:</p>
+        
+        <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+          Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+          such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+          An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      </div>
+      <div class="col-sm-4">
+        <img src="https://i.ytimg.com/vi/jUibaPTXSHk/hqdefault.jpg" />
+      </div>
+    </div>
   </div>
 )
 
